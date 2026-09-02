11c78ff2d2b5a056096ea6ff02b9dbf8453dea89
Charan6924 <altmrcharbot@gmail.com>
2026-09-02 15:07:51 -0500

    arrays, references, structs and enims

---
+++ src/bin/reference.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let mut s = String::from("hello");
+
+    change(&mut s);
+
+    println!("{s}");
+}
+
+fn change(string: &mut String){
+    string.push_str(", world");
+}
---
+++ src/bin/struct.rs
@@ -0,0 +1,22 @@
+fn main() {
+    struct User {
+        active : bool,
+        username : String,
+        email : String,
+        sign_in_count : u16,
+    }
+
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = User {
+        email : String::from("idk@gmail.com"),
+        ..user1
+    };
+}
